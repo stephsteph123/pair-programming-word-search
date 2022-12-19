dbad9abc19f8003ce3b0ee112c8062fd92d87154
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-12-18 23:08:15 -0500

    update with wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,18 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
-
+    let result = []; 
+    for (let x = 0; x < letters[0].length; x++) { 
+        const column = [] 
+        for (let y=0; y < letters.length; y++) { 
+            column.push(letters[y][x]); 
+        }
+        result.push(column)
+        }
+        const VertJoin = result.map(ls => ls.join(''))
+        for (let y of VertJoin) {
+            if (y.includes(word)) return true
+        };
+        return false
+};
 module.exports = wordSearch
